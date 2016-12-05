4ad7ab8172b09439fa974096e77d92b4f8429c69
MACHEN <mc-tattoo@Mc-tattoo.local>
2016-12-06 00:50:16 +0800

    start

---
+++ sell-app/src/components/header/header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header">
+    我是头部
+  </div>
+</template>
+
+<script style="text/ecmascript-6">
+  export default{};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
