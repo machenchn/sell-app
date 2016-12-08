e1a608223e0f9e21226e36e3814d3a418641a16a
MACHEN <mc-tattoo@mc-tattoo.kingsoft.cn>
2016-12-08 15:22:28 +0800

    model

--- sell-app/src/main.js
+++ sell-app/src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
+import goods from './components/goods/goods';
+import seller from './components/seller/seller';
+import ratings from './components/ratings/ratings';
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+import 'common/stylus/index.styl';
+
+Vue.use(VueRouter);
+
+let app = Vue.extend(App);
+
+let router = new VueRouter({
+  linkActiveClass: 'active'
 });
+
+router.map({
+  '/goods': {
+    component: goods
+  },
+  '/seller': {
+    component: seller
+  },
+  '/ratings': {
+    component: ratings
+  }
+});
+
+router.start(app, '#app');
+
+router.go('/goods');
